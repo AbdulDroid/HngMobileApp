e68afebd2d79bbb2ea39f8d1905ec23c24f66af8
lanre01 <olanre12waju@gmail.com>
2017-09-28 20:05:57 +0100

    Initial commit

---
+++ app/src/main/java/hnginterns/hngmobileapp/FlightBookingActivity.java
@@ -0,0 +1,20 @@
+package hnginterns.hngmobileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlightBookingActivity extends AppCompatActivity {
+
+    //TODO: Add code to initialize views
+    //TODO: Add needed variables
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_booking);
+
+        // TODO: Initialize data binding
+    }
+
+    //TODO: Add logic for flight booking
+}
---
+++ app/src/main/java/hnginterns/hngmobileapp/LoginActivity.java
@@ -0,0 +1,19 @@
+package hnginterns.hngmobileapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+    //TODO(3): Add code to initialize views
+    //TODO(2): Add needed variables
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        // TODO(4): Initialize data binding
+    }
+
+    //TODO(5): Add logic for user login
+}
